19617212ed868da7558cce6c7f45648ba80c3e05
Chovin <ccarlson@docomopacific.com>
2016-07-15 16:28:32 +1000

    kinda working

--- CampsiteAlpha/Projects/PythonGitterBot/bot.py
+++ CampsiteAlpha/Projects/PythonGitterBot/bot.py
@@ -47,7 +47,7 @@ def trigger(*args, **kwargs):
 # helpers
 
 def say(msg):
-    return gitter.sendMessage(gitter.prev_room, msg)
+    return gitter.sendMessage(gitter.prev_room, str(msg))
 
 # helpers from Red-DiscordBot
 def read_json(filename):
@@ -61,38 +61,43 @@ def save_json(filename, data):
             separators=(',',' : '))
     return data
 
-def process_stream(stream):
-    msg = next(stream)
-    if not msg:
-        return
-    msg = json.loads(msg.decode('utf-8'))['text']
-    for line in stream:
-        pass  # consume stream
-    print(msg)
-    print(msg.startswith(prefix))
-    if msg.startswith(prefix):
-        cmsg = msg[len(prefix):]
-        cmd = cmsg.split()[0]
+# for now it seems there is no way to do multiple rooms. 
+# need to switch to async to fix or use multithreading (I'd prefer not)
+#
+# if <prefix>command_name in input, process command.
+# commands get rest of sentence as arg.
+# else if trigger in msg, process trigger
+# trigger can be part of a word in msg
+def process_stream(stream, room):
+    for msg in stream:
+        if not msg or msg == b' ':  # slow cycling of rooms
+            return
+        msg = json.loads(msg.decode('utf-8'))['text']
+        if not msg:  # slow cycling of rooms
+            return
+        if msg.startswith(prefix):
+            cmsg = msg[len(prefix):]
+            cmd = cmsg.split()[0]
 
-        if cmd in cmds['commands']:
-            try:
-                process_command(cmds['commands'][cmd],cmsg[len(cmd):])
-            except Exception as e:
-                print(traceback.print_exc())
-
-    else:
-        # going through triggers means we can have have multi-word triggers.
-        # but it also means execution of triggers is non-deterministic
-        # we're also deciding to only trigger on one per msg
-        # for now, hacking in on_message. change into event later
-        for kw in cmds['triggers'].keys():
-            if kw.lower() in msg.lower():
+            if cmd in cmds['commands']:
                 try:
-                    process_command(cmds['triggers'][kw],msg)
+                    process_command(cmds['commands'][cmd],cmsg[len(cmd):])
                 except Exception as e:
                     print(traceback.print_exc())
-                if kw:  # on_message hack
-                    break
+
+        else:
+            # going through triggers means we can have have multi-word triggers.
+            # but it also means execution of triggers is non-deterministic
+            # we're also deciding to only trigger on one per msg
+            # for now, hacking in on_message. change into event later
+            for kw in cmds['triggers'].keys():
+                if kw.lower() in msg.lower():
+                    try:
+                        process_command(cmds['triggers'][kw],msg)
+                    except Exception as e:
+                        print(traceback.print_exc())
+                    if kw:  # on_message hack
+                        break
 
 def process_command(func, argstr):
     sig = inspect.signature(func)
@@ -141,43 +146,60 @@ def _convert_to_bool(argument):
     else:
         raise RuntimeError(lowered + ' is not a recognised boolean option')
 
+def join(room):
+    if gitter.joinRoom(room) != None:
+        rooms.append(room)
+        save_json(_room_path,rooms)
+    else:
+        raise RuntimeError('Not able to join '+room)
+    return True
+
 # commands
 
 @command(name="hi")
 def foo():
-    print('hi')
+    say('hi')
 
 @command()
 def bar(s):
-    print(s + ' <-- wassat?')
+    say(s + ' <-- wassat?')
 
 @command()
 def ping():
     """you working?"""
     print('pinging')
-    say('pong')
+    say('Pong.')
 
 @command(name='sort')
 def _sort(*words):
     """sort list of things"""
-    print(' '.join(sorted(words)))
+    say(' '.join(sorted(words)))
 
 @command(name='sum')
 def _sum(*numbers : float):
     """add all the numbers together"""
-    print(sum(numbers))
+    say(sum(numbers))
 
 @command(aliases=['now'])
 def time():
     """current time"""
     from datetime import datetime
-    print(datetime.now())
+    say(datetime.now())
 
-@command(aliases=['exit'])
-def quit():
+@command(name='quit', aliases=['exit'])
+def _quit():
     """turn off the bot"""
     import sys
     sys.exit(0)
+
+@command(name='join')
+def _join(room):
+    try:
+        res = join(room)
+        if res:
+            print(room + ' joined')
+    except Exception as e:
+        print(e)
 
 @command()
 def tree(height : int=None):
@@ -185,21 +207,21 @@ def tree(height : int=None):
     if height is None:
         from random import randint
         height = randint(0,10)
-    print('\n'.join([' '*(height-i-1)+'*'*(2*i+1)
+    say('```py\n'.join([' '*(height-i-1)+'*'*(2*i+1)
             for i in range(0,height)])+
-            '\n'+' '*(height-1)+'H')
+            '\n'+' '*(height-1)+'H\n```')
 
 @command(name='prefix')
 def _prefix(_prefix_):
     prefix = _prefix_
 
-@command()
-def help(command_or_trigger=None):
+@command(name='help')
+def _help(command_or_trigger=None):
     """displays information about a command/trigger"""
     if command_or_trigger is None:
-        print('Commands:\n')
+        say('Commands:\n')
         list_commands()
-        print('\nTriggers:\n')
+        say('\nTriggers:\n')
         list_triggers()
     else:
         helps = []
@@ -208,9 +230,9 @@ def help(command_or_trigger=None):
                 helps.append(format_help(cmds[kind][command_or_trigger],
                         command_or_trigger))
         if not helps:
-            print(command_or_trigger+' is not a registered command or trigger')
+            say(command_or_trigger+' is not a registered command or trigger')
         else:
-            print('\n\n'.join(helps))
+            say('\n\n'.join(helps))
 
 def format_help(command, name):
     msg = '(trigger) ' if command.kind == 'trigger' else prefix;
@@ -231,11 +253,11 @@ def format_help(command, name):
 
 @command(name='commands')
 def list_commands():
-    print('\n'.join(sorted(cmds['commands'])))
+    say('\n'.join(sorted(cmds['commands'])))
 
 @command(name='triggers')
 def list_triggers():
-    print('\n'.join(sorted(cmds['triggers'])))
+    say('\n'.join(sorted(cmds['triggers'])))
 
 
 # triggers - take care. args passed are in the order the user typed them
@@ -243,7 +265,7 @@ def list_triggers():
 @trigger(aliases=['shit','bitch','trump'])
 def fuck():
     """no cursing D:"""
-    print('hey! no swearing!')
+    say('hey! no swearing!')
 
 @trigger(name="christmas tree")
 def c_tree(*msg):
@@ -258,7 +280,7 @@ def c_tree(*msg):
 
 @trigger(name='quit')
 def trigger_quit():
-    print("No don't leave D:")
+    say("No don't leave D:")
 
 # should make an event instead of hacking it like this
 @trigger(name='')
@@ -267,24 +289,24 @@ def on_message(*msg):
     if not random.randint(0,10):
         words = list(msg)
         random.shuffle(words)
-        print(' '.join(words))
+        say(' '.join(words))
 
 
 # input loop
 
-# if <prefix>command_name in input, process command.
-# commands get rest of sentence as arg.
-# else if trigger in msg, process trigger
-# trigger can be part of a word in msg
+# go through an listen to streams for all rooms
+# no nice way to do it non-async. very much needs a rewrite
 def loop():
     while True:
         for old in set(streams)-set(rooms):
             del streams[old]
-        for room in rooms:
+        for room in list(rooms):
             if room not in streams:
                 streams[room] = gitter.roomStream(room).iter_lines()
             gitter.prev_room = room
-            process_stream(streams[room])
+            print('Listening for messages in '+room)
+            process_stream(streams[room], room)
+                
 
 def login():
     token = ''
@@ -296,6 +318,7 @@ def login():
         print('No token file found. Please enter your Gitter API token.\n'
             'You can get it from here: https://developer.gitter.im/apps\n')
         token = input('Enter your token: ')
+    global gitter
     gitter = None
     while not gitter:
         try:
@@ -309,11 +332,7 @@ def login():
 
     while not rooms:
         room = input('Bot is not in a room.\nEnter a room name: ')
-        if gitter.joinRoom(room) != None:
-            rooms.append(room)
-            save_json(_room_path,rooms)
-        else:
-            print('Not able to join '+room)
+        join(room)
 
 
     print('\nBot is on \o/\nIn these servers: {}\n{} commands registered\n'
@@ -324,7 +343,6 @@ def login():
 
 
 rooms = read_json(_room_path) if os.path.isfile(_room_path) else []
-streams = {}
 
 gitter = login()
 loop()
